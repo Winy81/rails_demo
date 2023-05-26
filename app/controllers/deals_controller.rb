ddf216cc092316a6f2aa72cbd3f1b50cb7456ebd
Winy81 <Adam.Nemeth@FootballPools.com>
2023-05-26 23:35:53 +0100

    Handling action around get_deals_of_city

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,8 +1,25 @@
 class DealsController < ApplicationController
 
+  ACCEPTED_CITIES = ['Glasgow', 'Edinburgh', 'Dundee']
+
   def index
-    @data = 'blah'
+    @message = 'Please add the name of the City for get deals'
   end
 
+  def get_deals_of_city
+
+    if ACCEPTED_CITIES.include?(city_deals_search_params[:name_of_city])
+      @deals = 'deals'
+    else
+      flash[:alert] = 'Deals for this city are not available at this moment'
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def city_deals_search_params
+    params.require(:city).permit(:name_of_city)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   get 'deals/index'
+  post 'deals/get_deals_of_city'
 
   root to: 'deals#index'
 end
