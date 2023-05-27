e2a48741d7651323c043a39ec6ae70ead4899017
Winy81 <Adam.Nemeth@FootballPools.com>
2023-05-27 20:48:28 +0100

    Clear the code of layouts

--- app/helpers/deals_helper.rb
+++ app/helpers/deals_helper.rb
@@ -1,2 +1,13 @@
 module DealsHelper
+
+  def type_checked_deal(record)
+    if record.has_key?("deal")
+      return record["deal"]
+    elsif record.has_key?("event")
+      return record["event"]
+    elsif record.has_key?("shop")
+      return record["shop"]
+    end
+  end
+
 end
